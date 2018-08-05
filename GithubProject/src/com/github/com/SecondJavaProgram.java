4a976841116a7ad3ecac2fb87522a705912b5dae
sudheer <sudheer@LAPTOP-NMF0BT2M>
2018-08-05 23:43:32 +0530

    Adding Second Java Program

---
+++ GithubProject/src/com/github/com/SecondJavaProgram.java
@@ -0,0 +1,10 @@
+package com.github.com;
+
+public class SecondJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Sudheer Adding Second Program into Github...!");
+
+	}
+
+}
